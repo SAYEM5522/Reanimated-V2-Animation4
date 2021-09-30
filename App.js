f37189748dc3c1b25dd28738b38f584a95b4f1b8
SAYEM5522 <md1040582@gmail.com>
2021-09-30 09:27:28 +0600

    header add

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './Component/Header';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
---
+++ Component/Header.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+const ImageWidth=190;
+const ImageHeight=190;
+const BorderRadious=100
+const Header = () => {
+ 
+  return (
+    <View style={styles.Container}>
+    <SimpleLineIcons name="arrow-left" size={24} style={{top:15}} color="black" />
+    <View style={styles.ImageView}>
+      <Image
+      source={{uri:'https://t3.ftcdn.net/jpg/04/08/99/00/360_F_408990068_A8QzYIfgChv66j71u5eavcIKA6NC2ML3.jpg'}}
+      style={styles.Image}
+      />
+    </View>
+    <Feather name="download"style={{top:15}} size={24} color="black" />
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+  Image:{
+    width:ImageWidth,
+    height:ImageHeight,
+    borderRadius:BorderRadious,
+    top:45,
+    alignSelf:'center'
+  },
+  Container:{
+    display:"flex",
+    flexDirection:'row',
+    justifyContent:"space-around"
+  },
+  ImageView:{
+    width:ImageWidth+80,
+    height:ImageHeight+90,
+    backgroundColor:"#b2bdcf",
+    borderBottomLeftRadius:150,
+    borderBottomRightRadius:150
+  }
+})
